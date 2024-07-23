aea6da4d1e8b9f7266148623ba2cddff9b2ffd3b
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-07-23 12:53:54 +0530

    Update UploadToVimeo.router.js

--- routes/UploadToVimeo.router.js
+++ routes/UploadToVimeo.router.js
@@ -80,8 +80,16 @@ console.log(req.files.video[0].originalname)
     function (error) {
       console.log('Failed because: ' + error)
       res.status(500).json({success : false})
-      fs.unlinkSync(`./temp/${uniqueVideoName}`)
-      console.log('deleted successfully')
+
+      try{
+        fs.unlinkSync(`./temp/${uniqueVideoName}`)
+        console.log('deleted successfully')
+
+
+      }catch(e){
+        console.log("file already deleted unable to deleted")
+      }
+
 
     }
   )
