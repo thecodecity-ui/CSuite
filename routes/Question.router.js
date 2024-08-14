ac9d4938322ecaf75e8659c91ad82c8d1a7628aa
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-08-14 11:41:21 +0530

    Update Question.router.js

--- routes/Question.router.js
+++ routes/Question.router.js
@@ -12,4 +12,64 @@ router.get('/', async (req, res) => {
   }
 });
 
+
+router.get('/:id', async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.id);
+    if (!question) {
+      return res.status(404).json({ message: 'Section not found' });
+    }
+    res.json(question);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  const { sections } = req.body;
+  
+  const newQuestion = new Question({
+    sections: sections
+  });
+
+  try {
+    const savedQuestion = await newQuestion.save();
+    res.status(201).json(savedQuestion);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedQuestion = await Question.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    
+    if (!updatedQuestion) {
+      return res.status(404).json({ message: 'Section not found' });
+    }
+    
+    res.json(updatedQuestion);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedQuestion = await Question.findByIdAndRemove(req.params.id);
+    if (!deletedQuestion) {
+      return res.status(404).json({ message: 'Section not found' });
+    }
+    res.json({ message: 'Section deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
