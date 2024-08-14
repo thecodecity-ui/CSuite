32e43eaf698f36cb1e31605b3e816328c8b81e35
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-08-14 13:00:30 +0530

    Update Question.router.js

--- routes/Question.router.js
+++ routes/Question.router.js
@@ -13,63 +13,139 @@ router.get('/', async (req, res) => {
 });
 
 
+
 router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
   try {
-    const question = await Question.findById(req.params.id);
-    if (!question) {
-      return res.status(404).json({ message: 'Section not found' });
+    const questionDoc = await Question.findById(id);
+    if (!questionDoc) {
+      return res.status(404).json({ message: 'Document not found' });
     }
-    res.json(question);
+    res.status(200).json(questionDoc);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
 
-router.post('/', async (req, res) => {
-  const { sections } = req.body;
-  
-  const newQuestion = new Question({
-    sections: sections
-  });
+router.post('/:id/sections', async (req, res) => {
+  const { id } = req.params;
+  const newSection = req.body;
 
   try {
-    const savedQuestion = await newQuestion.save();
-    res.status(201).json(savedQuestion);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedQuestion = await Question.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    
-    if (!updatedQuestion) {
-      return res.status(404).json({ message: 'Section not found' });
+    const questionDoc = await Question.findById(id);
+    if (!questionDoc) {
+      return res.status(404).json({ message: 'Document not found' });
     }
-    
-    res.json(updatedQuestion);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
 
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedQuestion = await Question.findByIdAndRemove(req.params.id);
-    if (!deletedQuestion) {
-      return res.status(404).json({ message: 'Section not found' });
-    }
-    res.json({ message: 'Section deleted successfully' });
+    questionDoc.sections.push(newSection);
+    await questionDoc.save();
+    res.status(201).json(questionDoc);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
+
+router.put('/:id/sections/:sectionId/questions', async (req, res) => {
+  const { id, sectionId } = req.params;
+  const newQuestion = req.body;
+
+  try {
+    const questionDoc = await Question.findById(id);
+    if (!questionDoc) {
+      return res.status(404).json({ message: 'Document not found' });
+    }
+
+    const section = questionDoc.sections.find(s => s.section === parseInt(sectionId));
+    if (!section) {
+      return res.status(404).json({ message: 'Section not found' });
+    }
+
+    section.questions.push(newQuestion);
+    await questionDoc.save();
+    res.status(200).json(questionDoc);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+router.put('/:id/sections/:sectionId/questions/:questionIndex', async (req, res) => {
+  const { id, sectionId, questionIndex } = req.params;
+  const updatedQuestion = req.body;
+
+  try {
+    const questionDoc = await Question.findById(id);
+    if (!questionDoc) {
+      return res.status(404).json({ message: 'Document not found' });
+    }
+
+    const section = questionDoc.sections.find(s => s.section === parseInt(sectionId));
+    if (!section) {
+      return res.status(404).json({ message: 'Section not found' });
+    }
+
+    if (section.questions[questionIndex]) {
+      section.questions[questionIndex] = updatedQuestion;
+      await questionDoc.save();
+      res.status(200).json(questionDoc);
+    } else {
+      res.status(404).json({ message: 'Question not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+router.delete('/:id/sections/:sectionId/questions/:questionIndex', async (req, res) => {
+  const { id, sectionId, questionIndex } = req.params;
+
+  try {
+    const questionDoc = await Question.findById(id);
+    if (!questionDoc) {
+      return res.status(404).json({ message: 'Document not found' });
+    }
+
+    const section = questionDoc.sections.find(s => s.section === parseInt(sectionId));
+    if (!section) {
+      return res.status(404).json({ message: 'Section not found' });
+    }
+
+    if (section.questions[questionIndex]) {
+      section.questions.splice(questionIndex, 1);
+      await questionDoc.save();
+      res.status(200).json(questionDoc);
+    } else {
+      res.status(404).json({ message: 'Question not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+router.delete('/:id/sections/:sectionId', async (req, res) => {
+  const { id, sectionId } = req.params;
+
+  try {
+    const questionDoc = await Question.findById(id);
+    if (!questionDoc) {
+      return res.status(404).json({ message: 'Document not found' });
+    }
+
+    const sectionIndex = questionDoc.sections.findIndex(s => s.section === parseInt(sectionId));
+    if (sectionIndex !== -1) {
+      questionDoc.sections.splice(sectionIndex, 1);
+      await questionDoc.save();
+      res.status(200).json(questionDoc);
+    } else {
+      res.status(404).json({ message: 'Section not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 module.exports = router;
