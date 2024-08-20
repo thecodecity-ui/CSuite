e47f4eff17b353d8f98b226a7cd5a66cb0fa69a4
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-08-20 12:16:15 +0530

    Update Payment.router.js

--- routes/Payment.router.js
+++ routes/Payment.router.js
@@ -2,7 +2,7 @@ const {Router} = require('express')
 const paymentRouter = Router()
 const Payment = require('../models/Payment.model')
 const CourseDetail = require('../models/CourseDetails.model');
-const stripe = require("stripe")("sk_test_51PSLZRI8GvQfURhsFsx4HK1cRGqrc3LilDh0H57XWr44Zm2maO0XPfV7AZPHbRiBsqb1rcJXvCJZvtY61FLQcD8z00rm5v52VU");
+const stripe = require("stripe")("sk_test_51PUVZZRrG0ZkGYrrIq8xX3O1fcIQ4xrvYmHRM9m6oFSNjEZL0AcRnLmnAx7ZORfMLH0UwqEDQGlcFlfv7Hm7JJoN00nHLBHIxq");
 
 
 const { createCheckoutSession } = require('../models/Payment.model');
