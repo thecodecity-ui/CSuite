d902e14cd0000558912b1984cd7c5a8a9e9cf2a7
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-11-06 21:04:47 +0530

    Update User.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -151,7 +151,7 @@ const UserSchema = new mongoose.Schema({
     required: [false, 'Profile banner field required']
   },
   testScores: [TestScoreSchema],
-  elaTestScore: { type: Number },
+  elaTestScore: { type: Number,  default: 0 },
   emergencyContact: {
     type: EmergencyContactSchema,
     required: false
