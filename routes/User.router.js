0b2f42f5d2e6e9fabdfb2ae0721cbdcd7c8edd70
SARAN TG <113160601+Sarantg143@users.noreply.github.com>
2024-11-25 12:35:41 +0530

    Update User.router.js

--- routes/User.router.js
+++ routes/User.router.js
@@ -352,8 +352,7 @@ userRouter.post('/login', async (req, res) => {
   }
 });
 
-// Route to reset the user's password
- userRouter.put('/:id/resetpass', async (req, res) => {
+userRouter.put('/:id/resetpass', async (req, res) => {
   try {
     const { id } = req.params;
     const { newPassword } = req.body;
@@ -365,16 +364,16 @@ userRouter.post('/login', async (req, res) => {
     if (!user) {
       return res.status(404).json({ success: false, message: 'User not found' });
     }
-
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-    user.password = hashedPassword;
+    user.password = newPassword;
     await user.save();
+
     res.status(200).json({ success: true, message: 'Password reset successfully' });
   } catch (error) {
-   console.error('Error resetting password:', error);
+    console.error('Error resetting password:', error);
     res.status(500).json({ success: false, message: 'Internal server error', error: error.message });
   }
 });
+
 
 
 
